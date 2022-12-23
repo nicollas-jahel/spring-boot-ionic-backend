2cdedb24a5de4b9988a5e3d80db6b37b654fcf78
Nicollas Jahel <nicollasjahel@gmail.com>
2022-12-23 18:50:15 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/nicollasjahel/cursomc/services/CategoriaService.java
+++ src/main/java/com/nicollasjahel/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nicollasjahel.cursomc.domain.Categoria;
+import com.nicollasjahel.cursomc.dto.CategoriaDTO;
 import com.nicollasjahel.cursomc.repositories.CategoriaRepository;
 import com.nicollasjahel.cursomc.services.exceptions.DataIntegrityException;
 import com.nicollasjahel.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
 
